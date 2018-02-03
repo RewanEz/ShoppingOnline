d609ccf80fb1e0edd6706b9b29107e8e3ddea115
Rewan <rewanezz@gmail.com>
2018-02-03 18:04:15 +0200

    Business Layers

---
+++ OnlineShopping/App_Code/ProductBL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopping
+{
+    public class ProductBL
+    {
+        public static DataTable GetAll()
+        {
+            string query = "select * from Product";
+            return DBManager.ExecuteQuery(query);
+        }
+
+        public static DataTable GetByName(string _prodName)
+        {
+            string query = $"select * from Product " +
+                $" where productName like '{_prodName}'";
+            return DBManager.ExecuteQuery(query);
+        }
+
+        public static int Insert(string _prodName, int _price, string _desc, int _catID, int _quantity, int _promotion, byte[] img)
+        {
+            string query = $"insert into product (productName,price," +
+                $"Description,categoryID,Image,Quantity,Promotion,deleted) " +
+                $"values({_prodName},{_price},{_desc},{_catID},{img},{_quantity}," +
+                $"{_promotion},{0})";
+            return DBManager.ExecuteNonQuery(query);
+        }
+
+        public static int Update(int _id, string _prodName, int _price, string _desc, int _catID, int _quantity, int _promotion, byte[] img)
+        {
+            string query = $"update product set" +
+                $" productName={_prodName},price={_price}," +
+                $"Description={_desc},categoryID={_catID},Image={img}," +
+                $"Quantity={_quantity},Promotion ={_quantity} " +
+                $"where productID={_id}";
+            return DBManager.ExecuteNonQuery(query);
+        }
+
+        public static int Deleted(int _id)
+        {
+            string query = $"update product set deleted = 1 " +
+                $"where productID = { _id }";
+            return DBManager.ExecuteNonQuery(query);
+        }
+    }
+}
---
+++ OnlineShopping/App_Code/UserBL.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace OnlineShopping
+{
+    public class UserBL
+    {
+        public static DataTable GetAllCurrent()
+        {
+            string query = "select * from Users where status='unblocked'";
+            return DBManager.ExecuteQuery(query);
+        }
+
+        public static DataTable GetAllNew()
+        {
+            string query = "select * from Users where status='blocked'";
+            return DBManager.ExecuteQuery(query);
+        }
+
+        public static DataTable GetUserByEmail(string _email)
+        {
+            string query = $"select * from users where Email = {_email}";
+            return DBManager.ExecuteQuery(query);
+        }
+        public static int Insert(string _fname, string _lname, string _email, string _Password, int _age, string _geneder)
+        {
+            string query = $"insert into users " +
+                $"(userFname,userLname,Email,Password,Gender,Age,Status)" +
+                $" values({_fname},{_lname},{_email},{_Password},{_geneder}," +
+                $"{_age},'blocked')";
+
+            return DBManager.ExecuteNonQuery(query);
+        }
+
+        public static int Update(int _id, string _fname, string _lname, string _email, string _Password, int _age, string _geneder)
+        {
+            string query = $"update Users set userFname={_fname}," +
+                $"userLname={_lname},Email={_email},Password={_Password}," +
+                $"Gender={_geneder},Age={_age} where userID = {_id}";
+
+            return DBManager.ExecuteNonQuery(query);
+        }
+
+        public static int UpdateStatus(int _id)
+        {
+            string query = $"update Users set Status='unblocked'" +
+                $" where userID = {_id}";
+
+            return DBManager.ExecuteNonQuery(query);
+        }
+
+        public static int Delete(int _id)
+        {
+            string query = $"update Users set Status='blocked'" +
+               $" where userID = {_id}";
+
+            return DBManager.ExecuteNonQuery(query);
+        }
+    }
+}
