22af1b04658683421316f9cbe482fecb7651d8d0
Rewan <rewanezz@gmail.com>
2018-02-03 14:22:27 +0200

    Initial Version

---
+++ OnlineShopping/App_Code/DBManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OnlineShopping
+{
+    public class DBManager
+    {
+        public static DataTable ExecuteQuery(string query)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["shoppingDB"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            DataTable tempTable = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(tempTable);
+            return tempTable;
+        }
+        public static int ExecuteNonQuery(string query)
+        {
+            int affectedRows;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["shoppingDB"].ConnectionString);
+            SqlCommand cmd = new SqlCommand(query, con);
+            try
+            {
+                con.Open();
+                affectedRows = cmd.ExecuteNonQuery();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return affectedRows;
+        }
+    }
+}
